7cb68135817b609215c14f09676660362acc6db4
winty <zhouwenting1@jd.com>
2016-11-11 15:07:22 +0800

    test connect to db

---
+++ configure/urlConfigures.js
@@ -0,0 +1,12 @@
+/**
+ * Created by winty on 2016/11/11.
+ */
+
+var urlConfig={
+    dbUrl:'mongodb://localhost/sharePictures',
+    dbServer:'127.0.0.1',
+    dbPort:'27017'
+}
+
+module.exports=urlConfig;
+
