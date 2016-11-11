5e688948f372935d9e5885f34b5ec614eb0a4ecf
winty <zhouwenting1@jd.com>
2016-11-11 19:39:19 +0800

    connect to db

---
+++ controllers/indexCtrl.js
@@ -0,0 +1,15 @@
+/**
+ * Created by winty on 2016/11/11.
+ */
+//var mongoose=require('mongoose');
+//var Test=mongoose.model('Test');
+//
+//module.exports.index=function(req,res){
+//    Test.findOne({name:"test"},function(error,test){
+//        if(error){
+//            console.log(error);
+//        }else if(test){
+//           // res.
+//        }
+//    })
+//}
