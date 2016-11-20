b0ab0081efec7f549fd62806e4159ff543baf534
winty <18814127231@163.com>
2016-11-20 18:23:03 +0800

    start pic

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -1,14 +1,8 @@
 /**
  * Created by winty on 2016/11/20.
  */
-var fs = require('fs');
-var Busboy = require('busboy');
-var mongo = require('mongodb');
-var Grid = require('gridfs-stream');
-var db = new mongo.Db('mongoImage', new mongo.Server("127.0.0.1", 27017), {safe: false});
-var gfs;
-var util = require('util');
-var bodyParser = require('body-parser');
+var mongoose=require('mongoose');
+var share=mongoose.model('share');
 
 
 module.exports.doDeclare=function(req,res){
--- models/model.js
+++ models/model.js
@@ -2,10 +2,77 @@
  * Created by winty on 2016/11/11.
  */
 var mongoose=require('mongoose');
-var dbUrl=require('../configure/urlConfigures');
 
-var testSchema=new mongoose.Schema({
-    name:String
+//用户Schema定义
+var userSchema = new mongoose.Schema({
+    userName: String, //用户名的类型为字符串
+    password: {
+        type: String, //密码的类型为字符串
+        require: true //非空
+    },
+    created: {
+        type: Date, //创建日期的类型为日期类型
+        "default": Date.now //默认值为创建时间
+    },
+    isAdmin:{
+        type:Boolean,
+        "default":false
+    },
+    isPublic:{
+        type:Boolean,
+        "default":true
+    },
+    myShares:{
+        shares:[{type:mongoose.Schema.objectId,ref:'share'}]
+    },
+    myFocus:{
+        users:[{type:mongoose.Schema.objectId,ref:'user'}]
+    },
+    myCollections:{
+        shares:[{type:mongoose.Schema.objectId,ref:'share'}]
+    },
+    myLikes:{
+        shares:[{type:mongoose.Schema.objectId,ref:'share'}]
+    },
+    myComments:{
+        shares:[{type:mongoose.Schema.objectId,ref:'share'}]
+    }
 });
 
-mongoose.model('Test',testSchema);
+//分享Schema定义
+var shareSchema = new mongoose.Schema({
+    content: String, //分享内容为字符串
+    images:[],//存图片id
+    created: {
+        type: Date, //创建时间类型为日期类型
+        "default": Date.now //默认值为创建日期
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, //发布者的引用
+        ref: 'User', //引用自User Model
+        require: true //非空
+    },
+    comment: [commentSchema] //subDocument，子文档，即该分享的评论
+});
+
+//评论Schema定义
+var commentSchema = new mongoose.Schema({
+    content: {
+        type: String, //评论内容的类型为字符串
+        require: true //非空
+    },
+    created: {
+        type: Date, //创建日期的类型为日期类型
+        "default": Date.now //默认值为创建日期
+    },
+    subComment: [this], //自评论的类型为评论类型，也就是本身类型
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, //评论用户的引用
+        ref: 'User', //引用自User Model
+        require: true //非空
+    }
+});
+
+mongoose.model('user',userSchema);
+mongoose.model('share',shareSchema);
+mongoose.model('comment',commentSchema);
