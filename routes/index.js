98192a2631fb7b02ec7491105aad3e7c2edf1311
winty <18814127231@163.com>
2016-11-20 23:42:25 +0800

    start pic

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,3 @@
-/**
- * Created by winty on 2016/11/12.
- */
 var express = require('express');
 var router = express.Router();
 
@@ -13,5 +10,17 @@ router.get('/index', function(req, res, next) {
 router.get('/user', function(req, res, next) {
     res.render('user');
 });
+
+/* 加载ceshi */
+router.get('/ceshi', function(req, res, next) {
+    res.render('test');
+});
+
+/* 加载分享页 */
+router.get('/share', function(req, res, next) {
+    res.render('share');
+});
+
+
 
 module.exports = router;
